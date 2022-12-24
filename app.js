ac0f7e6e920e458f848b19ad3d8ca3514f076155
stefifm <bruerastefania@gmail.com>
2022-12-23 22:02:49 -0300

    Encriptador Web

---
+++ app.js
@@ -0,0 +1,39 @@
+function encriptar() {
+  const texto = document.getElementById("textoCifrado").value.toLowerCase();
+
+  let textoEncriptado = texto.replace(/[eéèëê]/img, "enter");
+   textoEncriptado = textoEncriptado.replace(/[iíìïî]/img, "imes");
+   textoEncriptado = textoEncriptado.replace(/[aáàäâ]/img, "ai");
+   textoEncriptado = textoEncriptado.replace(/[oóòöô]/img, "ober");
+   textoEncriptado = textoEncriptado.replace(/[uúùüû]/img, "ufat");
+   textoEncriptado = textoEncriptado.replace(/[Ñ]/, "N"); 
+   textoEncriptado = textoEncriptado.replace(/[n]/, "n");
+   textoEncriptado = textoEncriptado.replace(/['|°¬!^`~"#$%&/()Çç=?¿{}_,.´+<>¡¨*:;]/img,'');
+
+  document.getElementById("textoDescifrado").innerHTML = textoEncriptado;
+
+  document.getElementById("btnCopiar").style.display = "show";
+  document.getElementById("btnCopiar").style.display = "flex";
+  document.getElementById("textoDescifrado").style.display = "show";
+  document.getElementById("textoDescifrado").style.display = "flex";
+  document.getElementById("titulo").style.display = "none";
+
+}
+
+function desencriptar() {
+  const frase = document.getElementById("textoCifrado").value.toLowerCase();
+
+  let textoDesencriptado = frase.replace(/enter/img, "e");
+  textoDesencriptado = textoDesencriptado.replace(/imes/img, "i");
+  textoDesencriptado = textoDesencriptado.replace(/ai/img, "a");
+  textoDesencriptado = textoDesencriptado.replace(/ober/img, "o");
+  textoDesencriptado = textoDesencriptado.replace(/ufat/img, "u");
+  document.getElementById("textoDescifrado").innerHTML = textoDesencriptado;
+
+}
+
+function copiar() {
+  const contenido = document.querySelector("#textoDescifrado")
+  contenido.select()
+  document.execCommand("copy")
+}
